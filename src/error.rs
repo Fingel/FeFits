8d1e4fd12afb04c08a5539b52fc5364194920474
Austin Riba <austin@m51.io>
2026-05-02 21:25:48 -0700

    Add error enum

---
+++ src/error.rs
@@ -0,0 +1,21 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("I/O error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Invalid Block: {0}")]
+    InvalidBlock(String),
+    #[error("Invalid Card: {0}")]
+    InvalidCard(String),
+    #[error("InvalidHeader: {0}")]
+    InvalidHeader(String), // TODO: MissingKeyword, InvalidKeyword, etc
+    #[error("InvalidHDU: {0}")]
+    InvalidHDU(String), // TODO split into structured variants: UnknownXtension type
+    #[error("Unsupported Feature: {0}")]
+    UnsupportedFeature(String),
+    #[error("Checksum Mismatch: expected {expected}, got {actual}")]
+    ChecksumMismatch { expected: u32, actual: u32 },
+    #[error("Type mismatch: {0}")]
+    TypeMismatch(String), // TODO Split into structured variants: Actual/expected
+}
