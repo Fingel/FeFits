e3a3e4db9b130fae3c7b48849cae66ebe20e66bd
Austin Riba <austin@m51.io>
2026-05-02 23:01:30 -0700

    Block type

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,6 @@
 use thiserror::Error;
 
+#[allow(dead_code)]
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("I/O error: {0}")]
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
 mod error;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+/// Section 2.2;
+/// A sequence of 2880 eight-bit bytes aligned on
+/// 2880-byte boundaries in the FITS file, most commonly either
+/// a header block or a data block. Special records are another
+/// infrequently used type of FITS block. This block length was
+/// chosen because it is evenly divisible by the byte and word
+/// lengths of all known computer systems at the time FITS was
+/// developed in 1979
+pub struct Block([u8; 2880]);
+
+impl Block {
+    pub fn zeroed() -> Self {
+        Block([0; 2880])
+    }
+
+    pub fn as_bytes(&self) -> &[u8] {
+        &self.0
+    }
+
+    pub fn as_bytes_mut(&mut self) -> &mut [u8] {
+        &mut self.0
+    }
+
+    pub fn records(&self) -> impl Iterator<Item = &[u8; 80]> {
+        self.0.as_chunks::<80>().0.iter()
+    }
 }
 
 #[cfg(test)]
@@ -9,8 +33,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn thirty_six_records() {
+        let block = Block::zeroed();
+        let records: Vec<&[u8; 80]> = block.records().collect();
+        assert_eq!(records.len(), 36);
     }
 }
